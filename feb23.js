79bcd4c9c90bd28e61173515fa126650a2d41ada
caseyyee <caseyyee.ca@gmail.com>
2019-03-06 17:00:27 -0800

    cleanup sketch

--- feb23.js
+++ feb23.js
@@ -1,14 +1,10 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util').random;
-const palettes = require('nice-color-palettes')
 
-global.TWEEN = require('@tweenjs/tween.js');
 global.THREE = require('three');
 global.WEBVR = require('./js/WebVR.js');
 
-require('three/examples/js/utils/GeometryUtils.js');
 require('three/examples/js/controls/OrbitControls');
-
 
 const settings = {
   context: 'webgl',
@@ -17,11 +13,11 @@ const settings = {
 
 const sketch = ({ context }) => {
   const scene = new THREE.Scene();
+
   const camera = new THREE.PerspectiveCamera(70, 1, 1, 5000);
 
-  // Setup controls
   const controls = new THREE.OrbitControls(camera, context.canvas);
-  controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+  controls.enableDamping = true;
   controls.dampingFactor = 0.25;
   controls.screenSpacePanning = false;
   controls.minDistance = 70;
@@ -29,28 +25,122 @@ const sketch = ({ context }) => {
   controls.minPolarAngle = Math.PI / 4;
   controls.maxPolarAngle = Math.PI / 2;
 
-  const setDefaultCameraPosition = () => {
+  scene.add(new THREE.AmbientLight('#cfcfcf'));
+  const light = new THREE.DirectionalLight('#fff', 1);
+  light.position.set(-100, 100, 40);
+  scene.add(light);
+
+  let isPresentingVr = false;
+  let wasPresentingVr = false;
+
+  // skew matrix
+  const Syx = 0, Szx = 0, Sxy = 0, Szy = 0.2, Sxz = 0, Syz = 0;
+  const skewMat = new THREE.Matrix4();
+  skewMat.set(  1,   Syx,  Szx,  0,
+              Sxy,     1,  Szy,  0,
+              Sxz,   Syz,   1,   0,
+                0,     0,   0,   1);
+
+  // stars
+  const starLength = 60;
+  const starSpeed = 30;
+  const stars = [];
+  const starBounds = new THREE.Vector3(300, 300, 300);
+  const startOffset = new THREE.Vector3(0, 0, 300);
+  const starGeo = new THREE.Geometry();
+  starGeo.vertices.push(new THREE.Vector3(0, 0, starLength));
+  starGeo.vertices.push(new THREE.Vector3(0, 0, -starLength));
+  const starMat = new THREE.LineBasicMaterial({
+    color: '#ffffff',
+    linewidth: 1 });
+
+  // ships
+  const ships = [];
+  const shipBlast = [];
+  const numShips = 5;
+  const shipXspacing = 30;
+  const shipZspacing = 15;
+  const shipYspacing = 18;
+  const fleetWidth = numShips * shipXspacing;
+
+  // ship body
+  const shipGeo = new THREE.ConeGeometry(15, 25, 3);
+  const shipMat = new THREE.MeshPhongMaterial({
+    flatShading: true,
+    color: '#0DA9FF' });
+  const shipMesh = new THREE.Mesh(shipGeo, shipMat);
+  const shipLineGeo = new THREE.EdgesGeometry(shipMesh.geometry);
+  const shipLineMat = new THREE.LineBasicMaterial({
+    color: '#00D7FF',
+    linewidth: 1 });
+  const shipLines = new THREE.LineSegments(shipLineGeo, shipLineMat);
+  shipLines.geometry.applyMatrix(skewMat);
+  shipMesh.scale.z = 0.2;
+  shipMesh.rotation.x = Math.PI / 2;
+  shipMesh.rotation.z = Math.PI;
+  shipMesh.geometry.applyMatrix(skewMat);
+  shipMesh.add(shipLines);
+
+  // ship canopy
+  const canopyGeo = new THREE.OctahedronGeometry(5);
+  const canopyMat = new THREE.MeshBasicMaterial({ color: '#393939' });
+  const canopyMesh = new THREE.Mesh(canopyGeo, canopyMat);
+  const canopyLineGeo = new THREE.EdgesGeometry(canopyMesh.geometry);
+  const canopyLineMat = new THREE.LineBasicMaterial({
+    color: '#00D7FF',
+    linewidth: 1 });
+  const canopyLines = new THREE.LineSegments(canopyLineGeo, canopyLineMat);
+  canopyMesh.add(canopyLines);
+  canopyMesh.scale.z = 0.3;
+  canopyMesh.scale.x = 0.5;
+  canopyMesh.position.z = -1;
+  canopyMesh.position.y = 0.69;
+  canopyMesh.rotation.x = -0.06;
+  canopyMesh.rotation.x += Math.PI / 2;
+
+  // ship boosters
+  const boosterGeo = new THREE.CylinderGeometry(2, 2, 10, 8);
+  const boosterMat = new THREE.MeshPhongMaterial({
+    flatShading: true,
+    color: '#690078' });
+  const boosterMesh = new THREE.Mesh(boosterGeo, boosterMat);
+  const boosterLineGeo = new THREE.EdgesGeometry(boosterMesh.geometry);
+  const boosterLineMat = new THREE.LineBasicMaterial({
+    color: '#FF00B3',
+    linewidth: 1 });
+  const boosterLines = new THREE.LineSegments(boosterLineGeo, boosterLineMat);
+  boosterLines.geometry.applyMatrix(skewMat);
+  boosterMesh.geometry.applyMatrix(skewMat);
+  boosterMesh.add(boosterLines);
+  boosterMesh.position.y = 2;
+  boosterMesh.position.z = 11;
+  boosterMesh.rotation.x = Math.PI / 2;
+  const boosterSpacing = 4;
+
+  // ship jet blast
+  const blastGeo = new THREE.OctahedronGeometry(2);
+  const blastMat = new THREE.MeshBasicMaterial({ color: '#FBE89A' });
+  const blastMesh = new THREE.Mesh(blastGeo, blastMat);
+  blastMesh.position.z = 16;
+  blastMesh.scale.z = 2;
+  blastCount = 10;
+
+  const setCameraPosition = () => {
     camera.position.set(-80, 20, -70);
   }
 
-  setDefaultCameraPosition();
-
-  const palette = random.pick(palettes);
-
   const makeSky = () => {
-    var geometry = new THREE.SphereBufferGeometry( 900, 32, 32 );
-    var material = new THREE.ShaderMaterial({
+    const gradientMat = new THREE.ShaderMaterial({
       uniforms: {
         color1: {
-          value: new THREE.Color("#181F4B")
+          value: new THREE.Color('#181F4B')
         },
         color2: {
-          value: new THREE.Color("#000000")
+          value: new THREE.Color('#000000')
         }
       },
       vertexShader: `
         varying vec2 vUv;
-
         void main() {
           vUv = uv;
           gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
@@ -64,191 +154,62 @@ const sketch = ({ context }) => {
           gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
         }
       `,
-      side: THREE.DoubleSide,
+      side: THREE.BackSide,
       wireframe: false
     });
-    var mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    scene.add(new THREE.Mesh(
+      new THREE.SphereBufferGeometry(900, 32, 32),
+      gradientMat));
   };
-
-  const jetBlast = [];
 
   const makeShip = () => {
     const shipGroup = new THREE.Group();
-    const Syx = 0, Szx = 0, Sxy = 0, Szy = 0.2, Sxz = 0, Syz = 0;
-    const skewMat = new THREE.Matrix4();
-    skewMat.set(  1,   Syx,  Szx,  0,
-                Sxy,     1,  Szy,  0,
-                Sxz,   Syz,   1,   0,
-                  0,     0,   0,   1);
 
-    makeBoosters = () => {
-      const geometry = new THREE.CylinderGeometry(2, 2, 10, 8);
-      const material = new THREE.MeshPhongMaterial({
-        flatShading: true,
-        color: '#690078',
-        polygonOffset: true,
-        polygonOffsetFactor: 1, // positive value pushes polygon further away
-        polygonOffsetUnits: 1});
-      const mesh = new THREE.Mesh( geometry, material );
+    // make ship boosters
+    const boosterMesh1 = boosterMesh.clone();
+    const boosterMesh2 = boosterMesh.clone();
+    boosterMesh1.position.x = boosterSpacing;
+    boosterMesh2.position.x = -boosterSpacing;
+    shipGroup.add(boosterMesh1);
+    shipGroup.add(boosterMesh2);
 
-      const geo = new THREE.EdgesGeometry( mesh.geometry ); // or WireframeGeometry
-      const mat = new THREE.LineBasicMaterial( { color: '#FF00B3', linewidth: 1 } );
-      const wireframe = new THREE.LineSegments( geo, mat );
-      wireframe.geometry.applyMatrix(skewMat);
-      mesh.add( wireframe );
-
-      const mesh2 = mesh.clone();
-      mesh2.position.x = -5;
-      mesh2.position.y = 2;
-      mesh2.position.z = 11;
-      mesh2.rotation.x = Math.PI / 2;
-
-      mesh.position.x = 5;
-      mesh.position.y = 2;
-      mesh.position.z = 11;
-      mesh.rotation.x = Math.PI / 2;
-
-      mesh.geometry.applyMatrix(skewMat);
-
-      shipGroup.add(mesh);
-      shipGroup.add(mesh2);
+    // make ship blast
+    for (let i = 0; i < blastCount; i++) {
+      const blast = blastMesh.clone();
+      blast.position.z += Math.exp(i * 0.8);
+      blast.scale.z += Math.exp(i * 0.4);
+      shipBlast.push(blast);
+      shipGroup.add(blast);
     }
 
-    makeJetblast = () => {
-      const geometry = new THREE.OctahedronGeometry(2);
-      const material = new THREE.MeshBasicMaterial({
-        color: '#FBE89A',
-        polygonOffset: true,
-        // opacity: 1,
-        // transparent: true,
-        polygonOffsetFactor: 1, // positive value pushes polygon further away
-        polygonOffsetUnits: 1});
-      const mesh = new THREE.Mesh(geometry, material);
-
-      mesh.position.z = 16;
-      mesh.scale.z = 2;
-
-      for (let i = 0; i < 10; i++) {
-        const blast = mesh.clone();
-        blast.position.z += Math.exp(i * 0.8);
-        blast.scale.z += Math.exp(i * 0.4);
-        jetBlast.push(blast);
-        shipGroup.add(blast);
-      }
-    }
-
-    makeBody = () => {
-      const geometry = new THREE.ConeGeometry(15, 25, 3);
-      const material = new THREE.MeshPhongMaterial({
-        flatShading: true,
-        color: '#0DA9FF',
-        polygonOffset: true,
-        polygonOffsetFactor: 1, // positive value pushes polygon further away
-        polygonOffsetUnits: 1});
-      const mesh = new THREE.Mesh(geometry, material);
-
-      const geo = new THREE.EdgesGeometry(mesh.geometry); // or WireframeGeometry
-      const mat = new THREE.LineBasicMaterial({ color: '#00D7FF', linewidth: 1 });
-      const wireframe = new THREE.LineSegments(geo, mat);
-      wireframe.geometry.applyMatrix(skewMat);
-      mesh.scale.z = 0.2;
-      mesh.rotation.x = Math.PI / 2;
-      mesh.rotation.z = Math.PI;
-      mesh.geometry.applyMatrix(skewMat);
-      mesh.add(wireframe);
-
-      shipGroup.add(mesh);
-    }
-
-    makeCanopy = () => {
-      const geometry = new THREE.OctahedronGeometry(5);
-      const material = new THREE.MeshBasicMaterial({
-        color: '#393939',
-        polygonOffset: true,
-        polygonOffsetFactor: 1, // positive value pushes polygon further away
-        polygonOffsetUnits: 1});
-      const mesh = new THREE.Mesh( geometry, material );
-
-      const geo = new THREE.EdgesGeometry( mesh.geometry ); // or WireframeGeometry
-      const mat = new THREE.LineBasicMaterial( { color: '#00D7FF', linewidth: 1 } );
-      const wireframe = new THREE.LineSegments( geo, mat );
-      mesh.scale.z = 0.3;
-      mesh.scale.x = 0.5;
-      mesh.position.z = -1;
-      mesh.position.y = 0.69;
-      mesh.rotation.x = -0.06;
-
-      mesh.rotation.x += Math.PI / 2;
-      mesh.add( wireframe );
-
-      shipGroup.add(mesh);
-    }
-
-    makeJetblast();
-    makeBoosters();
-    makeBody();
-    makeCanopy();
-
+    // ship body and canopy
+    shipGroup.add(shipMesh.clone());
+    shipGroup.add(canopyMesh.clone());
     return shipGroup;
   }
 
-  const between = (min, max) => {
-    return Math.floor(Math.random()*(max-min+1)+min);
-  }
-
-  const stars = [];
-  const makeStars = (num) => {
-    let lowBound = -600;
-    let highBound = -300;
-    if (!num) {
-      num = 100; // initial
-      lowBound = -600;
-      highBound = 600;
-    }
-    var geometry = new THREE.BoxBufferGeometry( 0.01, 0.01, 90 );
-    var material = new THREE.MeshBasicMaterial( {color: 0xffffff, wireframe: true} );
-    var cube = new THREE.Mesh(geometry, material);
-
-    for (let i = 0; i < num; i++) {
-      const star = cube.clone();
-      star.position.set(between(-300, 300), between(-300, 300), between(lowBound, highBound));
+  const makeStars = (range, start, amount) => {
+    for (let i = 0; i < amount; i++) {
+      const star = new THREE.Line(starGeo, starMat);
+      star.position.set(
+        random.range(-range.x, range.x),
+        random.range(-range.y, range.y),
+        random.range(-range.z-start.z, range.z+start.z));
       stars.push(star);
       scene.add(star);
     }
   }
 
-  // main
-  makeSky();
-  const ships = [];
-  const numShips = 5;
-  const shipSpacing = 30;
-  const shipLead = 15;
-  const fleetWidth = numShips * shipSpacing;
-  for (let i = 0; i < numShips; i++) {
-    let ship = makeShip();
-    ship.position.x = i * (fleetWidth / numShips) - (fleetWidth / 2);
-    ship.position.z = -i * shipLead;
-    if (i > (numShips / 2)) {
-      ship.position.z = (numShips - i) * -shipLead;
-    }
-    ship.position.y = between(-18, 18);
-    ships.push(ship)
-    scene.add(ship);
-  }
-
-  makeStars();
-
   const render = (time) => {
     isPresentingVr = renderer.vr.getDevice() && renderer.vr.isPresenting();
     if (isPresentingVr && !wasPresentingVr) { // entering vr
-      // enable VR only after we enter, otherwise it conflicts with OrbitControls
+      // enable VR only after entering, otherwise it conflicts with OrbitControls
       renderer.vr.enabled = true;
       wasPresentingVr = true;
     }
 
     if (!isPresentingVr && wasPresentingVr) { // exiting vr
-      // explicitly call exitPresent on device so we get proper canvas resizing.
+      // explicitly call exitPresent on device so we get proper canvas resizing
       renderer.vr.getDevice().exitPresent();
       wasPresentingVr = false;
     }
@@ -257,6 +218,7 @@ const sketch = ({ context }) => {
       controls.update();
     }
 
+    // jiggle ships
     ships.forEach((ship, index) => {
       const i = index + 1;
       ship.position.x += Math.sin(time * 2 * i) / 25;
@@ -264,17 +226,18 @@ const sketch = ({ context }) => {
       ship.rotation.z += Math.sin(time * 2 * i) / 1500;
     });
 
+    // move stars
     stars.forEach((star, i) => {
-      star.position.z += 30;
-      if (star.position.z > 600) {
-        //console.log(i);
+      star.position.z += starSpeed;
+      if (star.position.z > starBounds.z) {
         stars.splice(i, 1);
         scene.remove(star);
-        makeStars(1);
+        makeStars(starBounds, startOffset, 1);
       }
     });
 
-    jetBlast.forEach((blast) => {
+    // jiggle ship blast
+    shipBlast.forEach((blast) => {
       blast.position.x = Math.random();
       blast.position.y = Math.random();
     });
@@ -282,39 +245,46 @@ const sketch = ({ context }) => {
     renderer.render(scene, camera);
   }
 
-  scene.add(new THREE.AmbientLight('#cfcfcf'));
-  const light = new THREE.DirectionalLight('#fff', 1);
-  light.position.set(-100, 100, 40);
-  scene.add(light);
-
+  // main
   const renderer = new THREE.WebGLRenderer(context);
-  // we use three.js renderer to drive render loop.
-  renderer.setAnimationLoop(render);
-
+  renderer.setAnimationLoop(render); // Use three.js renderer to drive render loop.
   document.body.appendChild(WEBVR.createButton(renderer));
 
-  let isPresentingVr = false;
-  let wasPresentingVr = false;
+  setCameraPosition();
+
+  makeStars(starBounds, new THREE.Vector3(0, 0, 0), 100);
+
+  makeSky();
+
+  // make ships
+  for (let i = 0; i < numShips; i++) {
+    let ship = makeShip();
+    ship.position.x = i * (fleetWidth / numShips) - (fleetWidth / 2);
+    ship.position.z = -i * shipZspacing;
+    if (i > (numShips / 2)) {
+      ship.position.z = (numShips - i) * -shipZspacing;
+    }
+    ship.position.y = random.range(-shipYspacing, shipYspacing);
+    ships.push(ship);
+    scene.add(ship);
+  }
 
   window.addEventListener('vrdisplaypresentchange', (e) => {
-    // disable vr after exit present due to conflicting with orbit controls.
     if (!e.display.isPresenting) {
+      // disable vr after VR presentation ends as to not conflict with OrbitControls.
       renderer.vr.enabled = false;
-      setDefaultCameraPosition();
+      setCameraPosition();
     }
   });
 
-  // draw each frame
   return {
     resize ({ pixelRatio, viewportWidth, viewportHeight }) {
-      console.log('resizing');
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight);
       camera.aspect = viewportWidth / viewportHeight;
       camera.updateProjectionMatrix();
     },
 
-    // Dispose of events & renderer for cleaner hot-reloading
     unload () {
       renderer.dispose();
     }
